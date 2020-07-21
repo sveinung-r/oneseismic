56481679cb3662d0f7950421ba48c19381f15345
sveinung-r <ssru@equinor.com>
2020-07-21 10:36:16 +0200

    kubernetes setup

--- kubernetes/kubernetes_setup.sh
+++ kubernetes/kubernetes_setup.sh
@@ -2,23 +2,20 @@
 
 source .env
 
-EMAIL=ssru@equinor.com
-NODE_COUNT=5
-RESOURCE_GROUP="OneSeismic"
-CLUSTER_NAME="OneCluserPlayground"
-
 az aks create \
+    -o table \
     -c $NODE_COUNT \
     -g $RESOURCE_GROUP \
     -n $CLUSTER_NAME \
+    --vnet-subnet-id ${SUBNET} \
+    --network-plugin azure \
+    --service-cidr ${SERVICE_CIDR} \
+    --dns-service-ip ${DNS_IP} \
     --generate-ssh-keys \
     --enable-managed-identity
 
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
 
-echo "Aks cluster created and credentials loaded"
-
-ACR_NAME=oneseismic.azurecr.io
 ACR_UNAME=$(az acr credential show -n $ACR_NAME --query="username" -o tsv)
 ACR_PASSWD=$(az acr credential show -n $ACR_NAME --query="passwords[0].value" -o tsv)
 
@@ -31,6 +28,7 @@ kubectl create secret docker-registry acr-secret \
 NODE_RESOURCE_GROUP=$(az aks list --query "[?name=='$CLUSTER_NAME'].nodeResourceGroup" -o tsv)
 IP_NAME="${CLUSTER_NAME}_public_ip"
 az network public-ip create \
+    -o table \
     -n $IP_NAME \
     --resource-group $NODE_RESOURCE_GROUP \
     --allocation-method Static \
@@ -40,6 +38,7 @@ IP=$(az network public-ip list --query "[?name=='$IP_NAME'].ipAddress" -o tsv)
 
 # Update public ip address with DNS name
 az network public-ip update \
+    -o table \
     --name $IP_NAME \
     --dns-name $(echo $CLUSTER_NAME | tr '[:upper:]' '[:lower:]') \
     --resource-group $NODE_RESOURCE_GROUP
@@ -52,14 +51,15 @@ metadata:
   name: mysecret
 type: Opaque
 data:
-  AZURE_STORAGE_ACCESS_KEY: $(echo -n $AZURE_STORAGE_ACCESS_KEY | base64 -w 0)
   AZURE_STORAGE_ACCOUNT: $(echo -n $AZURE_STORAGE_ACCOUNT | base64 -w 0)
   AZURE_MANIFEST_CONTAINER: $(echo -n $AZURE_MANIFEST_CONTAINER | base64 -w 0)
   AUTHSERVER: $(echo -n $AUTHSERVER | base64 -w 0)
   ISSUER: $(echo -n $ISSUER | base64 -w 0)
   AZURE_STORAGE_URL: $(echo -n $AZURE_STORAGE_URL | base64 -w 0)
-  API_SECRET: $(echo -n $API_SECRET | base64 -w 0)
   LOG_LEVEL: $(echo -n $LOG_LEVEL | base64 -w 0)
+  CLIENT_SECRET: $(echo -n $CLIENT_SECRET | base64 -w 0)
+  AUDIENCE: $(echo -n $AUDIENCE | base64 -w 0)
+  CLIENT_ID: $(echo -n $CLIENT_ID | base64 -w 0)
 " | kubectl apply -f -
 
 # Create a namespace for your ingress resources
@@ -71,59 +71,48 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 # Use Helm to deploy an NGINX ingress controller
 helm install nginx stable/nginx-ingress \
     --set controller.replicaCount=1 \
+    --set controller.config.large-client-header-buffers="4 64k" \
     --set controller.service.loadBalancerIP=${IP} \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
 
 # Install the CustomResourceDefinition resources separately
-#kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
 
 # Label the ingress-basic namespace to disable resource validation
 #kubectl label namespace ingress-basic cert-manager.io/disable-validation=true
 
 # Add the Jetstack Helm repository
-#helm repo add jetstack https://charts.jetstack.io
+helm repo add jetstack https://charts.jetstack.io
 
 # Update your local Helm chart repository cache
-#helm repo update
+helm repo update
 
 # Install the cert-manager Helm chart
-#helm install \
-#  cert-manager \
-#  --namespace ingress-basic \
-#  --version v0.13.0 \
-#  jetstack/cert-manager
-#
-#echo \
-#"
-#apiVersion: cert-manager.io/v1alpha2
-#kind: ClusterIssuer
-#metadata:
-#  name: letsencrypt
-#spec:
-#  acme:
-#    server: https://acme-v02.api.letsencrypt.org/directory
-#    email: $EMAIL
-#    privateKeySecretRef:
-#      name: letsencrypt
-#    solvers:
-#    - http01:
-#      ingress:
-#      class: nginx
-#" | kubectl apply -f -
+helm install \
+  cert-manager \
+  --version v0.13.0 \
+  jetstack/cert-manager
 
 DOMAIN_NAME=$(az network public-ip list --query "[?name=='$IP_NAME'].dnsSettings.fqdn" -o tsv)
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -out aks-ingress-tls.crt \
-    -keyout aks-ingress-tls.key \
-    -subj "/CN=$DOMAIN_NAME/O=aks-ingress-tls"
+sleep 1m
 
-kubectl create secret tls aks-ingress-tls \
-    --key aks-ingress-tls.key \
-    --cert aks-ingress-tls.crt
+cat kubernetes/oneseismic.yml | \
+    sed s/'PUBLIC_IP'/${IP}/ | \
+    sed s/'DOMAIN_NAME'/${DOMAIN_NAME}/ | \
+    sed s/'EMAIL'/${EMAIL}/ | \
+    kubectl apply --validate=false -f -
 
-cat oneseismic.yml | sed s/'PUBLIC_IP'/${IP}/ | sed s/'DOMAIN_NAME'/${DOMAIN_NAME}/ | kubectl apply -f -
+cat kubernetes/filebeat-kubernetes.yaml | \
+    sed s/elasticsearch_host/$ELASTICSEARCH_HOST/ | \
+    sed s/changeme/$ELASTICSEARCH_PASSWORD/ | \
+    kubectl apply --validate=false -f -
+
+cat kubernetes/metricbeat-kubernetes.yaml | \
+    sed s/elasticsearch_host/$ELASTICSEARCH_HOST/ | \
+    sed s/changeme/$ELASTICSEARCH_PASSWORD/ | \
+    kubectl apply --validate=false -f -
 
 echo "Your onesesmic cluster is up and running! Listening on: $IP"
 echo "Domain name: $DOMAIN_NAME"
--- test_client.py
+++ test_client.py
@@ -5,6 +5,77 @@ import json
 import argparse
 import sys
 import jwt
+import logging
+
+from dotenv import load_dotenv
+import requests
+import msal
+import atexit
+import os
+from pathlib import Path
+
+def get_token():
+    load_dotenv()
+
+    cache_file = os.path.join(Path.home(), ".oneseismic")
+    cache = msal.SerializableTokenCache()
+    if os.path.exists(cache_file):
+        cache.deserialize(open(cache_file, "r").read())
+    atexit.register(lambda: open(cache_file, "w").write(cache.serialize()))
+    config = {
+        "client_id": os.getenv("CLIENT_ID"),
+        "authority": os.getenv("AUTHSERVER"),
+        "scopes": ["https://storage.azure.com/user_impersonation"],
+    }
+
+    # Create a preferably long-lived app instance which maintains a token cache.
+    app = msal.PublicClientApplication(
+        config["client_id"], authority=config["authority"], token_cache=cache,
+    )
+
+    # The pattern to acquire a token looks like this.
+    result = None
+
+    # Note: If your device-flow app does not have any interactive ability, you can
+    #   completely skip the following cache part. But here we demonstrate it anyway.
+    # We now check the cache to see if we have some end users signed in before.
+    accounts = app.get_accounts()
+    if accounts:
+        logging.info("Account(s) exists in cache, probably with token too. Let's try.")
+        # print("Pick the account you want to use to proceed:")
+        # for a in accounts:
+        #     print(a)
+        # Assuming the end user chose this one
+        chosen = accounts[0]
+        # Now let's try to find a token in cache for this account
+        result = app.acquire_token_silent(config["scopes"], account=chosen)
+
+    if not result:
+        logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+
+        flow = app.initiate_device_flow(config["scopes"])
+        if "user_code" not in flow:
+            raise ValueError(
+                "Fail to create device flow. Err: %s" % json.dumps(flow, indent=4)
+            )
+
+        print(flow["message"])
+        sys.stdout.flush()  # Some terminal needs this to ensure the message is shown
+
+        # Ideally you should wait here, in order to save some unnecessary polling
+        # input(
+        #     "Press Enter after signing in from another device to proceed, CTRL+C to abort."
+        # )
+
+        result = app.acquire_token_by_device_flow(flow)  # By default it will block
+        # You can follow this instruction to shorten the block time
+        #    https://msal-python.readthedocs.io/en/latest/#msal.PublicClientApplication.acquire_token_by_device_flow
+        # or you may even turn off the blocking behavior,
+        # and then keep calling acquire_token_by_device_flow(flow) in your own customized loop.
+
+    if "access_token" in result:
+        # Calling graph using the access token
+        return {"Authorization": "Bearer " + result["access_token"]}
 
 def assemble_slice(parts, shape0, shape1):
     tiles = parts['tiles']
@@ -38,9 +109,7 @@ def main(argv):
     args = parser.parse_args(argv)
 
     url = f"{args.base_url}/{args.guid}/slice/{args.dim}/{args.lineno}"
-    token = jwt.encode({"exp": 1612534338}, args.secret, algorithm='HS256')
-    token = token.decode('UTF-8')
-    r = requests.get(url, headers={"Authorization": "Bearer " + str(token)}, verify='./aks-ingress-tls.crt')
+    r = requests.get(url, headers=get_token())
 
     print(r.status_code)
     parts = json.loads(r.content)
